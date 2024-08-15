8ff091a9c89db0801ec7b3d2d45b17d44de1b441
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2024-08-15 22:31:39 +0530

    submit sucess laravel11  with reactjs

--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -1,42 +1,67 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
 
-    const [formData,setFormData]=useState({
-name:'',
-email:'',
-password:'',
-password_confirmation:'',
-    })
+  async function handelRegister(e) {
+    e.preventDefault();
 
-    function handelRegister(e){
-        e.preventDefault();
-        console.log(formData)
-    }
+    const response = await fetch("/api/register", {
+      method: "post",
+      body: JSON.stringify(formData),
+    });
+
+    const data = response.json();
+    console.log(data);
+  }
+
   return (
-    <form 
-    onSubmit={handelRegister}
-    className='w-1/2 mx-auto space-y-6'>
-        <h1>register form</h1>
-   <div>
-    <input type="text" placeholder='Name'  value={formData.name}
-    onChange={(e)=>setFormData({})}
-    />
-   </div>
+    <form onSubmit={handelRegister} className="w-1/2 mx-auto space-y-6 mt-40">
+      <h1 className="text-center  text-3xl font-bold">register form</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          value={formData.name}
+          onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        />
+      </div>
 
-   <div>
-    <input type="text"  placeholder='Email' value={formData.email}/>
-   </div>
-   <div>
-    <input type="Password"  value={formData.password}/>
-   </div>
-   <div>
-    <input type="Confirm Password"  value={formData.password_confirmation}/>
-   </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Email"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+      </div>
+      <div>
+        <input
+          type="Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
+      </div>
+      <div>
+        <input
+          type="Confirm Password" placeholder="confirm password"
+          value={formData.password_confirmation}
+          onChange={(e) =>
+            setFormData({ ...formData, password_confirmation: e.target.value })
+          }
+        />
+      </div>
 
-<button className='primary-btn'>register</button>
+      <button className="primary-btn">register</button>
     </form>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
